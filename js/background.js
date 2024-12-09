d38c07e1e240d7aabd15b1a972622fc1a1788626
avtryanin <107047152+avtryanin@users.noreply.github.com>
2024-12-09 14:40:59 +0300

    Update background.js

--- js/background.js
+++ js/background.js
@@ -1,6 +1,6 @@
 //инициализация состояния при установке или обновлении
 chrome.runtime.onInstalled.addListener(async () => {
     chrome.storage.local.set({ isEnabled: true });
-    let url = chrome.runtime.getURL(`html/hello.html`);
+    let url = chrome.runtime.getURL('html/hello.html');
     let tab = await chrome.tabs.create({ url });
 });
