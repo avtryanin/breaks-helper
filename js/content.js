a4b067fc2cd28e8bbf4c940f6def1542545c9108
avtryanin <107047152+avtryanin@users.noreply.github.com>
2025-01-30 14:12:39 +0300

    Update content.js

--- js/content.js
+++ js/content.js
@@ -1,6 +1,14 @@
 // > Интерфейс
 
 
+//цвета
+const white = 'rgb(242,242,242)';
+const red = 'rgb(255,76,0)';
+const green = 'rgb(79,255,134)';
+const blue = 'rgb(178,214,255)';
+const beige = 'rgb(255,243,223)';
+const orange = 'rgb(189,102,0)';
+
 //стилизация компонентов
 function createStyledElement(tag, text, styles) {
 	const element = document.createElement(tag);
@@ -8,51 +16,116 @@ function createStyledElement(tag, text, styles) {
 	Object.assign(element.style, styles);
 	return element;
 }
+
 //основной компонент
 const dashboard = createStyledElement('div', '', {
-	padding: '10px',
-	bottom: '30px',
-	right: '30px',
+	padding: '4px',
+	bottom: '0px',
+	right: '16px',
 	position: 'fixed',
+	width: '279px',
 	display: 'flex',
 	flexWrap: 'wrap',
-	gap: '10px',
+	gap: '6px',
 	borderRadius: '10px',
-	backgroundColor: 'rgb(242,242,242)',
-	zIndex: '1000'
+	backgroundColor: white,
+	fontSize: '13px',
+	fontWeight: 'bold',
+	zIndex: '1000',
+	justifyContent: 'space-around',
+	alignItems: 'center',
 });
 
-//кол-во резолверов
-const resolvers = createStyledElement('div', 'resolvers', {
-	padding: '10px',
-	borderRadius: '10px',
-	fontWeight: 'bold',
-	backgroundColor: 'rgb(178,214,255)',
+//контейнер резолверов
+const resolvers = createStyledElement('div', '', {
+	minWidth: '132.5px',
+	height: '15px',
+	fontSize: '11px',
+	borderRadius: '11px',
 	flex: '1',
 	display: 'flex',
+	justifyContent: 'space-between',
+	alignItems: 'center',
+	backgroundColor: blue
+});
+
+//блоки со значениями статусов операторов
+const onshift = createStyledElement('div', 'OS', {
+	width: '22px',
+	height: '22px',
+	borderRadius: '11px',
+	display: 'flex',
 	justifyContent: 'center',
-	alignItems: 'center'
+	alignItems: 'center',
+	backgroundColor: green
+});
+
+const busy = createStyledElement('div', 'B', {
+	width: '22px',
+	height: '22px',
+	borderRadius: '11px',
+	display: 'flex',
+	justifyContent: 'center',
+	alignItems: 'center',
+	backgroundColor: red
+});
+
+const onbreak = createStyledElement('div', 'OB', {
+	width: '22px',
+	height: '22px',
+	borderRadius: '11px',
+	display: 'flex',
+	justifyContent: 'center',
+	alignItems: 'center',
+	backgroundColor: orange
+});
+
+const amount = createStyledElement('div', 'A', {
+	width: '22px',
+	height: '15px',
+	borderRadius: '11px',
+	display: 'flex',
+	justifyContent: 'center',
+	alignItems: 'center',
+	backgroundColor: blue,
 });
 
 //процент брейков
-const percent = createStyledElement('div', 'percent', {
-	padding: '10px',
-	borderRadius: '10px',
-	fontWeight: 'bold',
-	backgroundColor: 'rgb(79,255,134)',
+const percent = createStyledElement('div', '0 %', {
+	padding: '6px',
+	borderRadius: '8px',
+	backgroundColor: green,
 	flex: '1',
 	display: 'flex',
 	justifyContent: 'center',
 	alignItems: 'center'
 });
 
-//статус и время его посл. изменения
-const stateTimer = createStyledElement('div', 'state', {
-	padding: '10px',
-	fontWeight: 'bold',
+//контейнер для state и time
+const stateTimeContainer = createStyledElement('div', '', {
 	flexBasis: '100%',
 	display: 'flex',
-	justifyContent: 'center',
+	fontSize: '12px',
+	gap: '6px',
+	justifyContent: 'space-around',
+	alignItems: 'center'
+});
+
+//статус польз.
+const state = createStyledElement('div', 'STATUS', {
+	fontWeight: 'bold',
+	display: 'flex',
+	flex: '1',
+	justifyContent: 'flex-end',
+	alignItems: 'center',
+});
+
+//время посл.изменения статуса
+const time = createStyledElement('div', 'TIMER', {
+	fontWeight: 'bold',
+	display: 'flex',
+	flex: '1',
+	justifyContent: 'flex-start',
 	alignItems: 'center'
 });
 
@@ -64,8 +137,14 @@ const stateTimer = createStyledElement('div', 'state', {
 function createComponents() {
 	document.body.appendChild(dashboard);
 	dashboard.appendChild(resolvers);
+	resolvers.appendChild(onshift);
+	resolvers.appendChild(busy);
+	resolvers.appendChild(onbreak);
+	resolvers.appendChild(amount);
 	dashboard.appendChild(percent);
-	dashboard.appendChild(stateTimer);
+	dashboard.appendChild(stateTimeContainer);
+	stateTimeContainer.appendChild(state);
+	stateTimeContainer.appendChild(time);
 }
 
 //удаление компонентов
@@ -79,24 +158,22 @@ function removeComponents() {
 // > Инициализация
 
 
+let url;
 let updateInterval;
-let user = '';
-let state = '';
 
 //инициализация состояния при загрузке
-chrome.storage.local.get(['isEnabled', 'timerValue'], (data) => {
+chrome.storage.local.get(['isEnabled', 'startTime', 'stateValue'], (data) => {
+	url = window.location.href;
 	if (data.isEnabled) {
-		if (data.timerValue) {
-			timer.seconds = data.timerValue; // Загружаем значение таймера
-			timer.displayTime(); // Отображаем загруженное значение
+		if (data.startTime) {
+			currentState = data.stateValue;
+			colorState(currentState)
+			timer.startTime = data.startTime;
 		}
 		updateInterval = setInterval(updateValues, 1000);
-		timer.start();
-		getUsername();
 		createComponents();
 	}
 });
-
 
 //слушатель тумблера
 chrome.storage.onChanged.addListener((changes, area) => {
@@ -104,9 +181,11 @@ chrome.storage.onChanged.addListener((changes, area) => {
 		const isEnabled = changes.isEnabled.newValue;
 		if (isEnabled) {
 			updateInterval = setInterval(updateValues, 1000);
+			timer.start()
 			createComponents();
 		} else {
 			clearInterval(updateInterval);
+			timer.stop()
 			removeComponents();
 		}
 	}
@@ -116,79 +195,88 @@ chrome.storage.onChanged.addListener((changes, area) => {
 // > Получение и обработка данных
 
 
-//получение логина польз.
-function getUsername() {
-	chrome.storage.sync.get(['username'], (result) => {
-		if (chrome.runtime.lastError) {
-			console.error('Error retrieving username from storage:', chrome.runtime.lastError);
-			return;
-		}
-
-		if (result.username) {
-			user = result.username;
-			console.log('Username loaded from storage:', user);
-			return;
-		}
-
-		try {
-			const username = document.getElementsByClassName(
-				'user-avatar_user__2Ul_N user-menu_avatar__1uxes'
-			)[0].firstChild.alt;
-
-			//сохранение в хранилище
-			chrome.storage.sync.set({ username: username }, function() {
-				if (chrome.runtime.lastError) {
-					throw new Error(chrome.runtime.lastError);
-				}
-				console.log('Username saved successfully!');
-				user = username;
-			});
-		} catch (error) {
-			console.error('Error occurred while getting or saving username:', error);
-		}
-	});
-}
+let login = '';
+let name = '';
+let currentState = '';
 
 //обновление значений
 async function updateValues() {
+	//обновление значения таймера
+	timer.updateElapsedTime();
+	//проверка изменения url
+	url = window.location.href;
+	if (url !== 'https://remote.sdc.yandex-team.ru/operators') {
+		clearInterval(updateInterval);
+		removeComponents();
+	}
 	//получение значений
 	try {
-		//получение username, если не удалось загрузить ранее
-		if (user === '') {
-			getUsername(); // Теперь у нас есть проверка, чтобы не вызывать getUsername повторно
+		//получение логина польз.
+		if (!login) {
+			login = document.getElementsByClassName(
+			"user-avatar_user__2Ul_N user-menu_avatar__1uxes"
+			)[0].children[0].alt;
 		}
-
+		
+		//парсинг информации
 		const operators = document.getElementsByClassName("operator_name__1XCUC");
 		const states = document.getElementsByClassName("operator_statusText__2-wrK operator_clickable__wtvNe");
+		const data = document.getElementsByClassName("operator_locations__3I0W4");
 
+		//счетчики
 		let sum = 0;
+		let shifts = 0;
+		let busies = 0;
 		let breaks = 0;
 
 		//обработка значений
 		for (let i = 0; i < operators.length; i++) {
+			//перебор операторов
 			const operator = operators[i].innerText;
-
-			//проверка исключений
+			//проверка исключенных операторов
 			const isExcluded = await checkExcluded(operator);
-			//подсчет резолверов
+			//подсчет кол-ва резолверов
 			if (!isExcluded) {
 				sum++;
-				//подсчет брейков
-				if (states[i].innerText === 'ON BREAK') {
-					breaks++;
+				//подсчет кол-ва брейков
+				if (states[i].innerText === 'ON SHIFT') {
+					shifts++;
+				} else if (states[i].innerText === 'BUSY') {
+					busies++
+				} else if (states[i].innerText === 'ON BREAK') {
+					breaks++
 				}
 			}
-			//определение статуса пользователя
-			if (operator === user) {
-				if (state !== states[i].innerText) {
-					state = states[i].innerText;
-					updateStateTimer(state);
+			//получение имени польз., если логин совпадает
+			if (login && !name) {
+				const parts = data[i].innerText.split('•');
+				operatorLogin = parts[1].trim();
+				if (login === operatorLogin) {
+					name = operator;
+				}
+			}
+			//обновление статуса польз.
+			if (name === operator) {
+				const newState = states[i].innerText;
+				if (newState !== currentState) {
+					colorState(newState);
+					currentState = newState;
+					timer.reset()
+					timer.start()
+					if (newState === undefined) {
+						currentState = ''// ?
+					}
 				}
 			}
 		}
-		resolvers.textContent = `На смене: ${sum}`;
-		//получение % брейков
-		getPercent(sum, breaks);
+		//вывод значений
+		onshift.textContent = `${shifts}`;
+		busy.textContent = `${busies}`;
+		onbreak.textContent = `${breaks}`;
+		amount.textContent = `${sum}`;
+		percent.textContent = `${getPercent(sum, breaks)} %`;
+		state.textContent = `${currentState}`;
+		timer.displayTime()
 	} catch (error) {
 		console.error('An error occurred while updating or processing the values:', error);
 	}
@@ -199,16 +287,16 @@ function getPercent(sum, breaks) {
 	let result;
 	if (sum !== 0) {
 		result = (100 / sum) * breaks;
-		if (result > 30) {
-			percent.style.backgroundColor = 'rgb(255,76,0)';
+		if (result >= 30) {
+			percent.style.backgroundColor = red;
 		} else {
-			percent.style.backgroundColor = 'rgb(79,255,134)';
+			percent.style.backgroundColor = green;
 		}
 	} else {
 		result = 0;
 	}
 	const roundedResult = result.toFixed(1);
-	percent.textContent = `${roundedResult} %`;
+	return roundedResult;
 }
 
 //проверка в списке исключений
@@ -231,42 +319,40 @@ function checkExcluded(operator) {
 	});
 }
 
-//обновление статуса и таймера
-function updateStateTimer(newState) {
-	if (state !== newState) { // Сброс только при изменении состояния
-		timer.reset(newState);
-		saveTimerValue(); // Сохраняем значение таймера при изменении состояния
-	}
-	state = newState; // Обновляем текущее состояние
-
-	if (state === `ON SHIFT`) {
-		stateTimer.style.color = `rgb(79,255,134)`;
-	} else if (state === `ON BREAK`) {
-		stateTimer.style.color = `orange`; // изменить цвет
-	} else if (state === `BUSY`) {
-		stateTimer.style.color = `rgb(255,76,0)`;
+//цвет статуса польз.
+function colorState(newState) {
+	if (newState === 'ON SHIFT') {
+		state.style.color = green;
+	} else if (newState === 'ON BREAK') {
+		state.style.color = orange;
+	} else if (newState === 'BUSY') {
+		state.style.color = red;
 	} else {
-		stateTimer.style.color = `white`;
+		state.style.color = white;
 	}
 }
 
 
-//таймер
+// > таймер
+
+
 class Timer {
 	constructor() {
-		this.seconds = 0;
+		this.startTime = null;
+		this.elapsedTime = 0;
 		this.intervalId = null;
-		this.state = '';
 	}
 
 	//запуск
 	start() {
 		if (this.intervalId) return;
 
+		this.startTime = Date.now();
+		this.saveStartTime();
+
 		this.intervalId = setInterval(() => {
-			this.seconds++;
+			this.updateElapsedTime();
 			this.displayTime();
-			saveTimerValue();
 		}, 1000);
 	}
 
@@ -277,30 +363,37 @@ class Timer {
 	}
 
 	//сброс
-	reset(state) {
-		this.stop(); // Останавливаем таймер
-		this.seconds = 0; // Сбрасываем секунды
-		this.state = state; // Устанавливаем новое состояние
-		this.displayTime(); // Обновляем отображение
+	reset() {
+		this.stop();
+		this.elapsedTime = 0;
+		this.startTime = null;
+		this.displayTime();
 	}
 
 	//отображение
 	displayTime() {
-		const hours = String(Math.floor(this.seconds / 3600)).padStart(2, '0');
-		const minutes = String(Math.floor((this.seconds % 3600) / 60)).padStart(2, '0');
-		const seconds = String(this.seconds % 60).padStart(2, '0');
+		const hours = String(Math.floor(this.elapsedTime / 3600)).padStart(2, '0');
+		const minutes = String(Math.floor((this.elapsedTime % 3600) / 60)).padStart(2, '0');
+		const seconds = String(this.elapsedTime % 60).padStart(2, '0');
 	
-		stateTimer.textContent = `${this.state} ${hours}:${minutes}:${seconds}`;
+		time.textContent = `${hours}:${minutes}:${seconds}`;
+	}
+
+	//сохранение времени начала и статус в chrome.storage
+	saveStartTime() {
+		chrome.storage.local.set({ startTime: this.startTime, stateValue: currentState }, () => {
+			if (chrome.runtime.lastError) {
+				console.error('Error saving start time:', chrome.runtime.lastError);
+			}
+		});
+	}
+
+	//обновление прошедшего времени
+	updateElapsedTime() {
+		if (this.startTime) {
+			this.elapsedTime = Math.floor((Date.now() - this.startTime) / 1000);
+		}
 	}
 }
 
 const timer = new Timer();
-
-//сохранение значения таймера
-function saveTimerValue() {
-	chrome.storage.local.set({ timerValue: timer.seconds }, () => {
-		if (chrome.runtime.lastError) {
-			console.error('Error saving timer value:', chrome.runtime.lastError);
-		}
-	});
-}
